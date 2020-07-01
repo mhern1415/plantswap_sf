8dbe16762cdee765ce826129d65c6aef882fdca5
Mark <m.hern1415@gmail.com>
2020-07-01 14:22:07 -0700

    added for better organization

---
+++ frontend/src/components/posts/PostShowContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PostShow from './PostShow';
+///import ContactForm from './ContactForm';
+///import { connect } from 'react-redux';
+
+const PostShowContainer = () => {
+
+
+
+
+    return (
+
+     <div>
+            <h3>My One Post</h3>
+            <div><PostShow /></div>
+        </div>
+    )
+
+
+
+}
+
+export default PostShowContainer;
